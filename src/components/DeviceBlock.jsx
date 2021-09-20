88a66edbbecc3d0552c0d421e16abdc622bbacf9
bg-dev <nik.azizoff2016@gmail.com>
2021-09-20 12:04:02 +0300

    fix device_block form

--- src/components/DeviceBlock.jsx
+++ src/components/DeviceBlock.jsx
@@ -11,6 +11,7 @@ function DeviceBlock({device}){
                 src={"http://localhost:3000/" + device.imagePath + (device.colors ? (activeColor+1) : "1") + ".jpg"}
                 alt={device.name} />
               <h4 className="device-block__title">{device.name}</h4>
+              {device.colors && device.capacity &&
               <div className="device-block__selector">
                 <ul>
                   { device.colors &&
@@ -18,7 +19,7 @@ function DeviceBlock({device}){
                       key={index}
                       className={ (index === activeColor ? 'active ' : '') + "color" } 
                       onClick={() => setActiveColor(index)}
-                      style={{backgroundColor: '#'+item}}
+                      style={{backgroundColor: '#' + item}}
                       ></li>)
                   }
                 </ul>
@@ -34,6 +35,7 @@ function DeviceBlock({device}){
                   }
                 </ul>
               </div>
+              }
               <div className="device-block__bottom">
                 <div className="device-block__price">{
                 (device.price + (device.capacity ? device.capacity[activeCapacity].price : 0))
@@ -44,8 +46,7 @@ function DeviceBlock({device}){
                       d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                       fill="white" />
                   </svg>
-                  <span>Добавить</span>
-                  <i>1</i>
+                  <span> Добавить</span>
                 </div>
               </div>
             </div>
