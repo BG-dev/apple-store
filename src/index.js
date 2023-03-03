5a11bb0f9643f3d63b720eb8e5f7073ef5bf4521
Nikita <nik.azizoff2016@gmail.com>
2023-03-03 23:04:26 +0300

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './scss/app.scss';
-import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom'
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import "bootstrap/dist/css/bootstrap.css";
+import "./scss/app.scss";
 
-import store from './redux/store'
+import store from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Provider store={store}>
-        <App/>
+        <App />
       </Provider>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
